0775cec74b31f021dac5c6d5d77f6a54c496e199
lopth <64159266+lopth@users.noreply.github.com>
2020-08-06 23:19:17 -0300

    Create scapy_ping.py

---
+++ scapy_ping.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+from scapy.all import *
+
+conf.verb = 0
+
+print "Hosts que respondem ICMP:"
+
+for ip in range (1, 255):
+        iprange = "192.168.0.%s" %ip
+        pIP = IP(dst=iprange)
+        pacote = pIP/ICMP()
+        resp, noresp = sr(pacote,timeout=1)
+
+        for resposta in resp:
+                print resposta[1][IP].src
